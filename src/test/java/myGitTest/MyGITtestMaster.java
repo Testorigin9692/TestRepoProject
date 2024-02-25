18f764bfcb99f50b4c09eb4ebf05f915a896f2b7
Testorigin9692 <mailtomeorkiran@gmail.com>
2024-02-25 19:23:32 +0530

    my third commit

--- src/test/java/myGitTest/MyGITtestMaster.java
+++ src/test/java/myGitTest/MyGITtestMaster.java
@@ -5,6 +5,8 @@ public class MyGITtestMaster {
 	public static void main(String[] args) {
 		System.out.println("This is manager Branch");
 		System.out.println("i am a tester");
+		System.out.println("you should work hard to get a job");
+		System.out.println("let us do it");
 	}
 
 }
